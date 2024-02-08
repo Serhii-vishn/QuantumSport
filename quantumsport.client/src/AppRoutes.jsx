7d42ed028022d00a6da550edc263ab29abc993c8
Ira Bondar <irabondar1012@gmail.com>
2024-02-08 17:59:51 +0200

    create component Header and its styles

--- quantumsport.client/src/AppRoutes.jsx
+++ quantumsport.client/src/AppRoutes.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import MainPage from './pages/MainPage/MainPage';
+import MainPage from "./pages/MainPage/MainPage";
 
 function AppRoutes() {
-    return (
-        <Routes>
-            <Route path="/" element={<MainPage />} />
-        </Routes>
-    );
+  return (
+    <Routes>
+        <Route path="/" element={<MainPage/>}/>
+        <Route path="/services" element={<MainPage/>}/>
+        <Route path="/team" element={<MainPage/>}/>
+        <Route path="/sections" element={<MainPage/>}/>
+        <Route path="/blog" element={<MainPage/>}/>
+        <Route path="/aboutUs" element={<MainPage/>}/>
+    </Routes>
+  );
 }
 
 export default AppRoutes;
--- quantumsport.client/src/Components/Header/Header.jsx
+++ quantumsport.client/src/Components/Header/Header.jsx
@@ -1,9 +1,60 @@
 import React from 'react';
+import styles from "./Header.module.scss";
+import { ReactComponent as Logo } from "../../img/Logo.svg";
+import { NavLink } from 'react-router-dom';
 
 function Header() {
-    return (<>
-    snkvfhgf
-    </>);
+    return (
+        <div className={styles.header}>
+            <NavLink to="/" className={styles.headerLogo}>
+                <Logo className={styles.logoImg} />
+                <h2 className={styles.logoTitle}>ENERGYGYM</h2>
+            </NavLink>
+            <nav className={styles.headerNav}>
+                <ul className={styles.headerNavList}>
+                    <li className={styles.headerNavListItem}>
+                        <NavLink to="/" className={styles.headerNavListItemLink}>
+                            головна
+                        </NavLink></li>
+                    <li className={styles.headerNavListItem}>
+                        <NavLink to="/services" className={styles.headerNavListItemLink}>
+                            послуги
+                        </NavLink>
+                        <ul  className={styles.headerNavListItemInfo}>
+                            <li><NavLink className={styles.infoLink}>Записатись на тренування</NavLink></li>
+                            <li><NavLink className={styles.infoLink}>Замовити індивідуальну програму тренувань</NavLink></li>
+                            <li><NavLink className={styles.infoLink}>Замовити індивідуальний план харчування</NavLink></li>
+                        </ul>
+                    </li>
+                    <li className={styles.headerNavListItem}>
+                        <NavLink to="/team" className={styles.headerNavListItemLink}>
+                            команда
+                        </NavLink>
+                        {/* <ul className={styles.headerNavListItemInfo}>
+
+                        </ul> */}
+                        </li>
+                    <li className={styles.headerNavListItem}>
+                        <NavLink to="/sections" className={styles.headerNavListItemLink}>
+                            спортивні секції
+                        </NavLink>
+                        {/* <div className={styles.headerNavListItemInfo}>
+
+                        </div> */}
+                        </li>
+                    <li className={styles.headerNavListItem}>
+                        <NavLink to="/blog" className={styles.headerNavListItemLink}>
+                            блог
+                        </NavLink></li>
+                    <li className={styles.headerNavListItem}>
+                        <NavLink to="/aboutUs" className={styles.headerNavListItemLink}>
+                            про energygym
+                        </NavLink></li>
+                </ul>
+                <button className={styles.headerNavBtn}>МОЇ ТРЕНУВАННЯ</button>
+            </nav>
+        </div>
+    );
 }
 
 export default Header;
