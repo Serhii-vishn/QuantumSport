87855026e4d3ab2ce49b0ca70152611d8f4eb814
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2024-02-08 12:58:15 +0200

    implement ModelsTests/ Added ClientTests

---
+++ QuantumSport.API/QuantumSport.UnitTests/ModelsTests/ClientsTests.cs
@@ -0,0 +1,11 @@
+﻿namespace QuantumSport.UnitTests
+{
+    public class ClientsTests
+    {
+        [Fact]
+        public void ValidateClient()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
