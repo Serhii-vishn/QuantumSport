a8ac546a587e4cb61f6353382a81c36a401ff004
Ira Bondar <irabondar1012@gmail.com>
2024-02-10 17:24:28 +0200

    change styles of MainPage

--- quantumsport.client/src/pages/MainPage/MainPage.jsx
+++ quantumsport.client/src/pages/MainPage/MainPage.jsx
@@ -9,10 +9,10 @@ function MainPage() {
         <div className={styles.photoSection}>
             <img className={styles.photoSectionImg} src={photo} alt="Фото головної сторінки" />
             <div className={styles.photoSectionInfo}>
-                <h1 className={styles.photoSectionInfoTitle}>Тренуйте своє тіло та розум</h1>
+                <h1 className={styles.photoSectionInfoTitle}>Тренуй своє тіло та розум</h1>
                 <p className={styles.photoSectionInfoText}>Приєднуйтесь до нас і відкривайте
                     новий рівень фізичної підготовки.</p>
-                <NavLink className={styles.photoSectionInfolink}>розпочни сьогодні!!!</NavLink>
+                <NavLink className={styles.photoSectionInfolink}>розпочни сьогодні!</NavLink>
             </div>
 
         </div>
