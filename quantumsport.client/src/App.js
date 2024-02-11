356f98a3308e5fd176e26a24e5518749193b8870
Ira Bondar <irabondar1012@gmail.com>
2024-02-11 19:40:44 +0200

    add section

--- quantumsport.client/src/App.js
+++ quantumsport.client/src/App.js
@@ -1,12 +1,14 @@
 import './App.scss';
 import AppRoutes from "./AppRoutes";
 import Header from './Components/Header/Header.jsx';
+import Footer from './Components/Footer/Footer.jsx';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <AppRoutes/>
+      <Footer/>
     </div>
   );
 }
---
+++ quantumsport.client/src/Components/Footer/Footer.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styles from "./Footer.module.scss";
+import { NavLink } from 'react-router-dom';
+import { ReactComponent as Logo } from "../../img/Logo.svg";
+
+function Footer() {
+
+
+    return (
+        <section className={styles.footer}>
+            <div className={styles.footerContacts}>
+                <h3 className={styles.footerContactsTitle}>Контакти</h3>
+                <p className={styles.footerContactsName}>Спортивний комплекс “EnergyGym”</p>
+                <address className={styles.footerContactsAddress}>м. Київ. Дніпровська набережна, 14</address>
+                <a className={styles.footerContactsPhone} href="tel:+380 (63) 080-80-80">+380 (63) 080-80-80</a>
+                <a className={styles.footerContactsPhone} href="tel:+380 (67) 080-80-80">+380 (67) 080-80-80</a>
+                <a className={styles.footerContactsPhone} href="tel:+380 (50) 080-80-80">+380 (50) 080-80-80</a>
+            </div>
+            <NavLink to="/" className={styles.logo}>
+                <Logo className={styles.logoImg} />
+                <h2 className={styles.logoTitle}>ENERGYGYM</h2>
+            </NavLink>
+            <div className={styles.footerSchedule}>
+                <h3 className={styles.footerScheduleTitle}>Графік роботи</h3>
+                <p className={styles.footerScheduleText}>Пн-пт......................7:00 - 22:00</p>
+                <p className={styles.footerScheduleText}>сб...........................9:00 - 19:00</p>
+                <p className={styles.footerScheduleText}>нд...........................9:00 - 15:00</p>
+            </div>
+        </section>
+    );
+}
+
+export default Footer;
