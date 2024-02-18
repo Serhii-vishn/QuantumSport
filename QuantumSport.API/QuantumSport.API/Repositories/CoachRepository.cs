536c9bbba1877bd436d87168b58579d45ece0584
DubBro <taras.melnyk.aka.krimyllex@gmail.com>
2024-02-18 23:01:43 +0200

    Create repository. Implement `GetAsync(int id)` and `ListAsync()`

---
+++ QuantumSport.API/QuantumSport.API/Repositories/CoachRepository.cs
@@ -0,0 +1,30 @@
+﻿namespace QuantumSport.API.Repositories
+{
+    public class CoachRepository : ICoachRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CoachRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<CoachEntity?> GetAsync(int id)
+        {
+            var coach = await _context.Coaches
+                .Where(c => c.Id == id)
+                .Include(c => c.SportSections)
+                .ThenInclude(s => s.SportSection)
+                .SingleOrDefaultAsync();
+            return coach;
+        }
+
+        public async Task<IList<CoachEntity>> ListAsync()
+        {
+            return await _context.Coaches
+                .Include(c => c.SportSections)
+                .ThenInclude(s => s.SportSection)
+                .ToListAsync();
+        }
+    }
+}
