a57922b9489d7e2cb6badc00fabe9e9af02f2f1e
DubBro <taras.melnyk.aka.krimyllex@gmail.com>
2024-02-19 03:02:41 +0200

    Fix `GetAsync()` methods of `UserRepository`

--- QuantumSport.API/QuantumSport.API/Repositories/Interfaces/IUserRepository.cs
+++ QuantumSport.API/QuantumSport.API/Repositories/Interfaces/IUserRepository.cs
@@ -2,8 +2,8 @@
 {
     public interface IUserRepository
     {
-        Task<UserEntity> GetAsync(int id);
-        Task<UserEntity> GetAsync(string phoneNumber);
+        Task<UserEntity?> GetAsync(int id);
+        Task<UserEntity?> GetAsync(string phoneNumber);
         Task<IList<UserEntity>> ListAsync();
         Task<int> AddAsync(UserEntity user);
         Task<int> UpdateAsync(UserEntity user);
